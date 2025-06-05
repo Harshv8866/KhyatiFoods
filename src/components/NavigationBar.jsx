f151126a42e843565f4785794b566acecf040090
Harsh V <harshvaghela7265@gmail.com>
2025-06-06 03:17:19 +0530

    Update: made some changes

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,51 +1,48 @@
 import React, { useState } from 'react';
-import { Navbar, Container } from 'react-bootstrap';
+import { Navbar, Nav, Container } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
 import logo from '../assets/Logo.png';
 import './NavigationBar.css';
 
 const NavigationBar = () => {
-  const [menuOpen, setMenuOpen] = useState(false);
+  const [expanded, setExpanded] = useState(false);
 
   const handleNavClick = () => {
-    setMenuOpen(false);
+    setExpanded(false); // Close the navbar when a link is clicked
   };
 
   return (
-    <>
-      <Navbar expand="lg" className="custom-navbar shadow-sm" sticky="top">
-        <Container>
-          <Navbar.Brand as={NavLink} to="/" className="d-flex align-items-center" onClick={handleNavClick}>
-            <img src={logo} alt="KhyatiFoods Logo" height="80" className="d-inline-block align-top me-2" />
-          </Navbar.Brand>
+    <Navbar
+      expand="lg"
+      className="custom-navbar shadow-sm"
+      sticky="top"
+      expanded={expanded}
+      onToggle={setExpanded}
+    >
+      <Container>
+        <Navbar.Brand as={NavLink} to="/" className="d-flex align-items-center" onClick={handleNavClick}>
+          <img
+            src={logo}
+            alt="KhyatiFoods Logo"
+            height="80"
+            className="d-inline-block align-top me-2"
+          />
+        </Navbar.Brand>
 
-          {/* Custom Toggle Button */}
-          <button
-            className="navbar-toggler"
-            type="button"
-            aria-label="Toggle navigation"
-            onClick={() => setMenuOpen(!menuOpen)}
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
+        <Navbar.Toggle aria-controls="main-navbar-nav" />
 
-          {/* Custom sliding menu */}
-          <div className={`custom-slide-menu ${menuOpen ? 'open' : ''}`}>
-            <nav className="nav flex-column">
-              <NavLink to="/" end className="nav-link" onClick={handleNavClick}>Home</NavLink>
-              <NavLink to="/about" className="nav-link" onClick={handleNavClick}>About Us</NavLink>
-              <NavLink to="/products" className="nav-link" onClick={handleNavClick}>Product</NavLink>
-              <NavLink to="/process" className="nav-link" onClick={handleNavClick}>Process</NavLink>
-              <NavLink to="/certificate" className="nav-link" onClick={handleNavClick}>Certificate</NavLink>
-              <NavLink to="/contact" className="nav-link" onClick={handleNavClick}>Contact</NavLink>
-            </nav>
-          </div>
-        </Container>
-      </Navbar>
-
-      {/* Backdrop */}
-      {menuOpen && <div className="menu-backdrop" onClick={() => setMenuOpen(false)}></div>}
-    </>
+        <Navbar.Collapse id="main-navbar-nav">
+          <Nav className="ms-auto nav-links">
+            <Nav.Link as={NavLink} to="/" end onClick={handleNavClick}>Home</Nav.Link>
+            <Nav.Link as={NavLink} to="/about" onClick={handleNavClick}>About Us</Nav.Link>
+            <Nav.Link as={NavLink} to="/products" onClick={handleNavClick}>Product</Nav.Link>
+            <Nav.Link as={NavLink} to="/process" onClick={handleNavClick}>Process</Nav.Link>
+            <Nav.Link as={NavLink} to="/certificate" onClick={handleNavClick}>Certificate</Nav.Link>
+            <Nav.Link as={NavLink} to="/contact" onClick={handleNavClick}>Contact</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 };
 
